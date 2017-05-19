d764820202e425c8195407bfe3be9f4d5c9099e7
PBSmoke <kittinamkorn@gmail.com>
2017-05-19 15:49:49 +0700

    #6

--- Application_Form/EvidenceListPicker.Designer.cs
+++ Application_Form/EvidenceListPicker.Designer.cs
@@ -29,109 +29,25 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.dgvEvidenceList = new DevComponents.DotNetBar.SuperGrid.SuperGridControl();
-            this.colEvidenceID = new DevComponents.DotNetBar.SuperGrid.GridColumn();
-            this.colEvidenceCode = new DevComponents.DotNetBar.SuperGrid.GridColumn();
-            this.colEvidenceName = new DevComponents.DotNetBar.SuperGrid.GridColumn();
-            this.colEvidenceType = new DevComponents.DotNetBar.SuperGrid.GridColumn();
-            this.colDetail = new DevComponents.DotNetBar.SuperGrid.GridColumn();
-            this.colPath = new DevComponents.DotNetBar.SuperGrid.GridColumn();
-            this.colCreatedBy = new DevComponents.DotNetBar.SuperGrid.GridColumn();
-            this.colCreatedDate = new DevComponents.DotNetBar.SuperGrid.GridColumn();
             this.tbEvidenceBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.applicationDS1 = new Application_Form.ApplicationData.ApplicationDS();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dgvEvidenceList = new System.Windows.Forms.DataGridView();
+            this.EvidenceID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EvidenceCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EvidenceName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EvidenceType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Detail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Path = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CreatedBy = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CreatedDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.tbEvidenceBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.applicationDS1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvidenceList)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dgvEvidenceList
-            // 
-            this.dgvEvidenceList.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvEvidenceList.FilterExprColors.SysFunction = System.Drawing.Color.DarkRed;
-            this.dgvEvidenceList.Location = new System.Drawing.Point(0, 55);
-            this.dgvEvidenceList.Name = "dgvEvidenceList";
-            // 
-            // 
-            // 
-            this.dgvEvidenceList.PrimaryGrid.Columns.Add(this.colEvidenceID);
-            this.dgvEvidenceList.PrimaryGrid.Columns.Add(this.colEvidenceCode);
-            this.dgvEvidenceList.PrimaryGrid.Columns.Add(this.colEvidenceName);
-            this.dgvEvidenceList.PrimaryGrid.Columns.Add(this.colEvidenceType);
-            this.dgvEvidenceList.PrimaryGrid.Columns.Add(this.colDetail);
-            this.dgvEvidenceList.PrimaryGrid.Columns.Add(this.colPath);
-            this.dgvEvidenceList.PrimaryGrid.Columns.Add(this.colCreatedBy);
-            this.dgvEvidenceList.PrimaryGrid.Columns.Add(this.colCreatedDate);
-            this.dgvEvidenceList.PrimaryGrid.DataSource = this.tbEvidenceBindingSource;
-            this.dgvEvidenceList.PrimaryGrid.MultiSelect = false;
-            this.dgvEvidenceList.PrimaryGrid.SelectionGranularity = DevComponents.DotNetBar.SuperGrid.SelectionGranularity.Row;
-            this.dgvEvidenceList.Size = new System.Drawing.Size(713, 380);
-            this.dgvEvidenceList.TabIndex = 124;
-            this.dgvEvidenceList.Text = "superGridControl1";
-            this.dgvEvidenceList.CellDoubleClick += new System.EventHandler<DevComponents.DotNetBar.SuperGrid.GridCellDoubleClickEventArgs>(this.dgvEvidenceList_CellDoubleClick);
-            // 
-            // colEvidenceID
-            // 
-            this.colEvidenceID.DataPropertyName = "EvidenceID";
-            this.colEvidenceID.HeaderText = "EvidenceID";
-            this.colEvidenceID.Name = "EvidenceID";
-            this.colEvidenceID.Visible = false;
-            // 
-            // colEvidenceCode
-            // 
-            this.colEvidenceCode.AutoSizeMode = DevComponents.DotNetBar.SuperGrid.ColumnAutoSizeMode.Fill;
-            this.colEvidenceCode.DataPropertyName = "EvidenceCode";
-            this.colEvidenceCode.FillWeight = 40;
-            this.colEvidenceCode.HeaderText = "EvidenceCode";
-            this.colEvidenceCode.Name = "EvidenceCode";
-            // 
-            // colEvidenceName
-            // 
-            this.colEvidenceName.AutoSizeMode = DevComponents.DotNetBar.SuperGrid.ColumnAutoSizeMode.Fill;
-            this.colEvidenceName.DataPropertyName = "EvidenceName";
-            this.colEvidenceName.FillWeight = 50;
-            this.colEvidenceName.HeaderText = "EvidenceName";
-            this.colEvidenceName.Name = "EvidenceName";
-            // 
-            // colEvidenceType
-            // 
-            this.colEvidenceType.AutoSizeMode = DevComponents.DotNetBar.SuperGrid.ColumnAutoSizeMode.Fill;
-            this.colEvidenceType.DataPropertyName = "EvidenceType";
-            this.colEvidenceType.FillWeight = 40;
-            this.colEvidenceType.HeaderText = "EvidenceType";
-            this.colEvidenceType.Name = "EvidenceType";
-            // 
-            // colDetail
-            // 
-            this.colDetail.AutoSizeMode = DevComponents.DotNetBar.SuperGrid.ColumnAutoSizeMode.Fill;
-            this.colDetail.DataPropertyName = "Detail";
-            this.colDetail.HeaderText = "Detail";
-            this.colDetail.Name = "Detail";
-            // 
-            // colPath
-            // 
-            this.colPath.DataPropertyName = "Path";
-            this.colPath.HeaderText = "Path";
-            this.colPath.Name = "Path";
-            this.colPath.Visible = false;
-            // 
-            // colCreatedBy
-            // 
-            this.colCreatedBy.DataPropertyName = "CreatedBy";
-            this.colCreatedBy.HeaderText = "CreatedBy";
-            this.colCreatedBy.Name = "CreatedBy";
-            this.colCreatedBy.Visible = false;
-            // 
-            // colCreatedDate
-            // 
-            this.colCreatedDate.DataPropertyName = "CreatedDate";
-            this.colCreatedDate.EditorType = typeof(DevComponents.DotNetBar.SuperGrid.GridDateTimeInputEditControl);
-            this.colCreatedDate.HeaderText = "CreatedDate";
-            this.colCreatedDate.Name = "CreatedDate";
-            this.colCreatedDate.Visible = false;
             // 
             // tbEvidenceBindingSource
             // 
@@ -145,8 +61,8 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.txtSearch);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
@@ -154,21 +70,112 @@
             this.groupBox1.TabIndex = 125;
             this.groupBox1.TabStop = false;
             // 
-            // button1
+            // btnSearch
             // 
-            this.button1.Location = new System.Drawing.Point(603, 17);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSearch.Location = new System.Drawing.Point(603, 17);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
-            // textBox1
+            // txtSearch
             // 
-            this.textBox1.Location = new System.Drawing.Point(28, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(569, 20);
-            this.textBox1.TabIndex = 0;
+            this.txtSearch.Location = new System.Drawing.Point(28, 19);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(569, 20);
+            this.txtSearch.TabIndex = 0;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // dgvEvidenceList
+            // 
+            this.dgvEvidenceList.AllowUserToAddRows = false;
+            this.dgvEvidenceList.AllowUserToDeleteRows = false;
+            this.dgvEvidenceList.AutoGenerateColumns = false;
+            this.dgvEvidenceList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvEvidenceList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEvidenceList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.EvidenceID,
+            this.EvidenceCode,
+            this.EvidenceName,
+            this.EvidenceType,
+            this.Detail,
+            this.Path,
+            this.CreatedBy,
+            this.CreatedDate});
+            this.dgvEvidenceList.DataSource = this.tbEvidenceBindingSource;
+            this.dgvEvidenceList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvEvidenceList.Location = new System.Drawing.Point(0, 55);
+            this.dgvEvidenceList.Name = "dgvEvidenceList";
+            this.dgvEvidenceList.ReadOnly = true;
+            this.dgvEvidenceList.Size = new System.Drawing.Size(713, 380);
+            this.dgvEvidenceList.TabIndex = 126;
+            this.dgvEvidenceList.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEvidenceList_CellDoubleClick);
+            // 
+            // EvidenceID
+            // 
+            this.EvidenceID.DataPropertyName = "EvidenceID";
+            this.EvidenceID.HeaderText = "EvidenceID";
+            this.EvidenceID.Name = "EvidenceID";
+            this.EvidenceID.ReadOnly = true;
+            this.EvidenceID.Visible = false;
+            // 
+            // EvidenceCode
+            // 
+            this.EvidenceCode.DataPropertyName = "EvidenceCode";
+            this.EvidenceCode.FillWeight = 30F;
+            this.EvidenceCode.HeaderText = "EvidenceCode";
+            this.EvidenceCode.Name = "EvidenceCode";
+            this.EvidenceCode.ReadOnly = true;
+            // 
+            // EvidenceName
+            // 
+            this.EvidenceName.DataPropertyName = "EvidenceName";
+            this.EvidenceName.FillWeight = 40F;
+            this.EvidenceName.HeaderText = "EvidenceName";
+            this.EvidenceName.Name = "EvidenceName";
+            this.EvidenceName.ReadOnly = true;
+            // 
+            // EvidenceType
+            // 
+            this.EvidenceType.DataPropertyName = "EvidenceType";
+            this.EvidenceType.FillWeight = 40F;
+            this.EvidenceType.HeaderText = "EvidenceType";
+            this.EvidenceType.Name = "EvidenceType";
+            this.EvidenceType.ReadOnly = true;
+            this.EvidenceType.Visible = false;
+            // 
+            // Detail
+            // 
+            this.Detail.DataPropertyName = "Detail";
+            this.Detail.HeaderText = "Detail";
+            this.Detail.Name = "Detail";
+            this.Detail.ReadOnly = true;
+            // 
+            // Path
+            // 
+            this.Path.DataPropertyName = "Path";
+            this.Path.HeaderText = "Path";
+            this.Path.Name = "Path";
+            this.Path.ReadOnly = true;
+            this.Path.Visible = false;
+            // 
+            // CreatedBy
+            // 
+            this.CreatedBy.DataPropertyName = "CreatedBy";
+            this.CreatedBy.HeaderText = "CreatedBy";
+            this.CreatedBy.Name = "CreatedBy";
+            this.CreatedBy.ReadOnly = true;
+            this.CreatedBy.Visible = false;
+            // 
+            // CreatedDate
+            // 
+            this.CreatedDate.DataPropertyName = "CreatedDate";
+            this.CreatedDate.HeaderText = "CreatedDate";
+            this.CreatedDate.Name = "CreatedDate";
+            this.CreatedDate.ReadOnly = true;
+            this.CreatedDate.Visible = false;
             // 
             // EvidenceListPicker
             // 
@@ -177,12 +184,14 @@
             this.ClientSize = new System.Drawing.Size(713, 435);
             this.Controls.Add(this.dgvEvidenceList);
             this.Controls.Add(this.groupBox1);
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
             this.Name = "EvidenceListPicker";
             this.Load += new System.EventHandler(this.EvidenceListPicker_Load);
             ((System.ComponentModel.ISupportInitialize)(this.tbEvidenceBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.applicationDS1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvidenceList)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -190,19 +199,28 @@
         #endregion
 
         private ApplicationData.ApplicationDS applicationDS;
-        private DevComponents.DotNetBar.SuperGrid.SuperGridControl dgvEvidenceList;
-        private DevComponents.DotNetBar.SuperGrid.GridColumn colEvidenceID;
-        private DevComponents.DotNetBar.SuperGrid.GridColumn colEvidenceCode;
-        private DevComponents.DotNetBar.SuperGrid.GridColumn colEvidenceName;
-        private DevComponents.DotNetBar.SuperGrid.GridColumn colEvidenceType;
-        private DevComponents.DotNetBar.SuperGrid.GridColumn colDetail;
-        private DevComponents.DotNetBar.SuperGrid.GridColumn colPath;
-        private DevComponents.DotNetBar.SuperGrid.GridColumn colCreatedDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEvidenceID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEvidenceCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEvidenceName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEvidenceType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDetail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPath;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCreatedDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCreatedBy;
         private System.Windows.Forms.BindingSource tbEvidenceBindingSource;
         private ApplicationData.ApplicationDS applicationDS1;
-        private DevComponents.DotNetBar.SuperGrid.GridColumn colCreatedBy;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dgvEvidenceList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EvidenceID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EvidenceCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EvidenceName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EvidenceType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Detail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Path;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CreatedBy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CreatedDate;
+
     }
 }
--- Application_Form/EvidenceListPicker.cs
+++ Application_Form/EvidenceListPicker.cs
@@ -25,7 +25,6 @@ namespace Application_Form
         #region
         ApplicationDS tblEvidence = new ApplicationDS();
         int SelectRowIndex = -1;
-        string EvidenceID = string.Empty;
         public ApplicationDS.tbEvidenceRow drEvidence = null;
         #endregion
 
@@ -46,7 +45,7 @@ namespace Application_Form
                 tblEvidence.Clear();
                 da.Fill(tblEvidence, "tbEvidence");
                 da.Dispose();
-                dgvEvidenceList.PrimaryGrid.DataSource = tblEvidence.tbEvidence;
+                dgvEvidenceList.DataSource = tblEvidence.tbEvidence;
                 //dgvEvidenceList.bi = tblEvidence.tbEvidence;
             }
             catch (Exception ex)
@@ -56,22 +55,72 @@ namespace Application_Form
             SelectRowIndex = -1;
         }
 
-
         private void EvidenceListPicker_Load(object sender, EventArgs e)
         {
             dbConString.Chk_ConnectionState();
             ShowData();
         }
 
-        private void dgvEvidenceList_CellDoubleClick(object sender, GridCellDoubleClickEventArgs e)
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            DoSearch();
+        }
+
+        private void dgvEvidenceList_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
             drEvidence = tblEvidence.tbEvidence.NewtbEvidenceRow();
-            drEvidence.EvidenceID = e.GridCell.GridRow[colEvidenceID].Value.ToString();
-            drEvidence.EvidenceCode = e.GridCell.GridRow[colEvidenceCode].Value.ToString();
-            drEvidence.EvidenceType = e.GridCell.GridRow[colEvidenceType].Value.ToString();
-            drEvidence.Detail = e.GridCell.GridRow[colDetail].Value.ToString();
-            drEvidence.Path = e.GridCell.GridRow[colPath].Value.ToString();
+            drEvidence.EvidenceID = dgvEvidenceList.Rows[e.RowIndex].Cells[colEvidenceID.Name].Value.ToString();
+            drEvidence.EvidenceCode = dgvEvidenceList.Rows[e.RowIndex].Cells[colEvidenceCode.Name].Value.ToString();
+            drEvidence.EvidenceName = dgvEvidenceList.Rows[e.RowIndex].Cells[colEvidenceName.Name].Value.ToString();
+            drEvidence.EvidenceType = dgvEvidenceList.Rows[e.RowIndex].Cells[colEvidenceType.Name].Value.ToString();
+            drEvidence.Detail = dgvEvidenceList.Rows[e.RowIndex].Cells[colDetail.Name].Value.ToString();
+            drEvidence.Path = dgvEvidenceList.Rows[e.RowIndex].Cells[colPath.Name].Value.ToString();
             this.Close();
+        }
+
+        private void DoSearch()
+        {
+            string Whereclause = string.Empty;
+            if (!string.IsNullOrEmpty(txtSearch.Text))
+            {
+                Whereclause = txtSearch.Text;
+            }
+            else
+            {
+                Whereclause = string.Empty;
+            }
+
+            try
+            {
+                string sqlTmp = "";
+                sqlTmp = "SELECT * FROM tbEvidence";
+                if (!string.IsNullOrEmpty(Whereclause))
+                {
+                    sqlTmp += " WHERE EvidenceCode LIKE '%" + Whereclause + "%' OR EvidenceName LIKE '%" + Whereclause + "%' OR EvidenceType LIKE '%" + Whereclause + "%' OR Detail LIKE '%" + Whereclause + "%'";
+                }
+                sqlTmp += " ORDER BY EvidenceCode";
+                DataSet Ds = new DataSet();
+                dbConString.Com = new SqlCommand();
+                dbConString.Com.CommandType = CommandType.Text;
+                dbConString.Com.CommandText = sqlTmp;
+                dbConString.Com.Connection = dbConString.mySQLConn;
+                SqlCommand cmd = new SqlCommand(sqlTmp, dbConString.mySQLConn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                tblEvidence.Clear();
+                da.Fill(tblEvidence, "tbEvidence");
+                da.Dispose();
+                dgvEvidenceList.DataSource = tblEvidence.tbEvidence;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            SelectRowIndex = -1;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            DoSearch();
         }
     }
 }
