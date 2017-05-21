d9382965c06b3b6b62eca60771371a0c7c163123
llzarunll <za_run@hotmail.com>
2017-05-21 10:01:06 +0700

    z

---
+++ EvidenceInfo.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Application_Form
+{
+    partial class EvidenceInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEvidenceCode = new System.Windows.Forms.TextBox();
+            this.txtEvidenceName = new System.Windows.Forms.TextBox();
+            this.txtDetail = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblAttachment = new System.Windows.Forms.Label();
+            this.btnPerview = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(10, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "รหัสพยาน/หลักฐาน";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(10, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "ชื่อพยาน/หลักฐาน";
+            // 
+            // txtEvidenceCode
+            // 
+            this.txtEvidenceCode.Location = new System.Drawing.Point(152, 57);
+            this.txtEvidenceCode.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtEvidenceCode.MaxLength = 50;
+            this.txtEvidenceCode.Name = "txtEvidenceCode";
+            this.txtEvidenceCode.Size = new System.Drawing.Size(174, 24);
+            this.txtEvidenceCode.TabIndex = 15;
+            // 
+            // txtEvidenceName
+            // 
+            this.txtEvidenceName.Location = new System.Drawing.Point(152, 89);
+            this.txtEvidenceName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtEvidenceName.MaxLength = 100;
+            this.txtEvidenceName.Name = "txtEvidenceName";
+            this.txtEvidenceName.Size = new System.Drawing.Size(385, 24);
+            this.txtEvidenceName.TabIndex = 16;
+            // 
+            // txtDetail
+            // 
+            this.txtDetail.Location = new System.Drawing.Point(152, 153);
+            this.txtDetail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDetail.MaxLength = 500;
+            this.txtDetail.Name = "txtDetail";
+            this.txtDetail.Size = new System.Drawing.Size(385, 251);
+            this.txtDetail.TabIndex = 19;
+            this.txtDetail.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 17);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "รายละเอียด";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(152, 121);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(216, 24);
+            this.txtPath.TabIndex = 21;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(374, 119);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(78, 28);
+            this.btnBrowse.TabIndex = 22;
+            this.btnBrowse.Text = "เบราว์...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblAttachment
+            // 
+            this.lblAttachment.AutoSize = true;
+            this.lblAttachment.Location = new System.Drawing.Point(10, 124);
+            this.lblAttachment.Name = "lblAttachment";
+            this.lblAttachment.Size = new System.Drawing.Size(77, 17);
+            this.lblAttachment.TabIndex = 23;
+            this.lblAttachment.Text = "เอกสารแนบ";
+            // 
+            // btnPerview
+            // 
+            this.btnPerview.Location = new System.Drawing.Point(461, 119);
+            this.btnPerview.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnPerview.Name = "btnPerview";
+            this.btnPerview.Size = new System.Drawing.Size(36, 28);
+            this.btnPerview.TabIndex = 122;
+            this.btnPerview.Text = "P";
+            this.btnPerview.UseVisualStyleBackColor = true;
+            this.btnPerview.Click += new System.EventHandler(this.btnPerview_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(503, 119);
+            this.btnclear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(34, 28);
+            this.btnclear.TabIndex = 123;
+            this.btnclear.Text = "C";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // EvidenceInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(548, 417);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnPerview);
+            this.Controls.Add(this.lblAttachment);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDetail);
+            this.Controls.Add(this.txtEvidenceName);
+            this.Controls.Add(this.txtEvidenceCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.MaximumSize = new System.Drawing.Size(564, 456);
+            this.MinimumSize = new System.Drawing.Size(532, 456);
+            this.Name = "EvidenceInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "พยาน/หลักฐาน";
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.txtEvidenceCode, 0);
+            this.Controls.SetChildIndex(this.txtEvidenceName, 0);
+            this.Controls.SetChildIndex(this.txtDetail, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.txtPath, 0);
+            this.Controls.SetChildIndex(this.btnBrowse, 0);
+            this.Controls.SetChildIndex(this.lblAttachment, 0);
+            this.Controls.SetChildIndex(this.btnPerview, 0);
+            this.Controls.SetChildIndex(this.btnclear, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEvidenceCode;
+        private System.Windows.Forms.TextBox txtEvidenceName;
+        private System.Windows.Forms.RichTextBox txtDetail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblAttachment;
+        private System.Windows.Forms.Button btnPerview;
+        private System.Windows.Forms.Button btnclear;
+    }
+}
---
+++ EvidenceInfo.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Application_Form.ApplicationData;
+using Application_Service;
+using Application_Service.ClassService;
+using DocExp;
+    
+namespace Application_Form
+{
+    public partial class EvidenceInfo : BaseInfo
+    {
+        public EvidenceInfo()
+        {
+            InitializeComponent();
+        }
+
+        #region
+        public string EvidenceID = string.Empty;
+        ApplicationDS tblEvidence = new ApplicationDS();
+        string sqlTmp = string.Empty;
+        #endregion
+
+        protected override void DoLoadForm()
+        {
+            btnPerview.Enabled = false;
+
+            switch (FormState.ToLower())
+            {
+                case "new":
+                    EvidenceID = string.Empty;
+                    break;
+                case "edit":
+                    DoLoadData(EvidenceID);
+                    break;
+                default:
+                    EvidenceID = string.Empty;
+                    break;
+            }
+        }
+
+        private void DoLoadData(string EvidenceID)
+        {
+            #region tbUser
+            try
+            {
+                string sqlTmp = "";
+                sqlTmp = "SELECT * FROM tbEvidence WHERE EvidenceID = '" + EvidenceID + "'";
+                DataSet Ds = new DataSet();
+                dbConString.Com = new SqlCommand();
+                dbConString.Com.CommandType = CommandType.Text;
+                dbConString.Com.CommandText = sqlTmp;
+                dbConString.Com.Connection = dbConString.mySQLConn;
+                dbConString.Com.Parameters.Clear();
+                SqlCommand cmd = new SqlCommand(sqlTmp, dbConString.mySQLConn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                tblEvidence.Clear();
+                da.Fill(tblEvidence, "tbEvidence");
+                da.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            #endregion
+            if (tblEvidence.tbEvidence.Rows.Count > 0)
+            {
+                EvidenceID = tblEvidence.tbEvidence[0].EvidenceID;
+                txtEvidenceCode.Text = tblEvidence.tbEvidence[0].EvidenceCode;
+                txtEvidenceName.Text = tblEvidence.tbEvidence[0].EvidenceName;
+                txtPath.Text = tblEvidence.tbEvidence[0].Path;
+                //cboEvidenceType.Text = tblEvidence.tbEvidence[0].EvidenceType;
+                txtDetail.Text = tblEvidence.tbEvidence[0].Detail;
+            }
+
+            if (string.IsNullOrEmpty(txtPath.Text))
+                btnPerview.Enabled = false;
+            else
+                btnPerview.Enabled = true;
+        }
+
+        private void DoCheckData()
+        {
+            if (string.IsNullOrEmpty(txtEvidenceCode.Text))
+            {
+                MessageBox.Show("รหัสพยาน/หลักฐานไม่สามารถเป็นค่าว่างได้ กรุณาป้อนข้อมูล", "Warning", MessageBoxButtons.OK);
+                Success = false;
+                txtEvidenceCode.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtEvidenceName.Text))
+            {
+                MessageBox.Show("ชื่อพยาน/หลักฐานไม่สามารถเป็นค่าว่างได้ กรุณาป้อนข้อมูล", "Warning", MessageBoxButtons.OK);
+                Success = false;
+                txtEvidenceName.Focus();
+                return;
+            }
+
+            //if (cboEvidenceType.SelectedIndex == -1)
+            //{
+            //    MessageBox.Show("Evidence Type is Empty", "Warning", MessageBoxButtons.OK);
+            //    Success = false;
+            //    cboEvidenceType.Focus();
+            //    return;
+            //}
+        }
+
+        protected override void DoSave()
+        {
+            Success = true;
+            DoCheckData();
+
+            if (!Success)
+                return;
+
+            //Check Duplicate
+            if (Success == true && tblEvidence.tbEvidence.Rows.Count > 0)
+            {
+                Success = Utilities.CheckDuplicateNotInOldValues(tblEvidence.tbEvidence.TableName,
+                                                                tblEvidence.tbEvidence.EvidenceCodeColumn.ColumnName,
+                                                                txtEvidenceCode.Text,
+                                                                tblEvidence.tbEvidence[0].EvidenceCode);
+                if (!Success)
+                {
+                    MessageBox.Show("รหัสพยาน/หลักฐานไม่สามารถซ้ำได้ กรุณาป้อนข้อมูลใหม่", "คำเตือน", MessageBoxButtons.OK);
+                    Success = false;
+                    return;
+                }
+            }
+            else
+            {
+                Success = Utilities.CheckDuplicate(tblEvidence.tbEvidence.TableName,
+                                                  tblEvidence.tbEvidence.EvidenceCodeColumn.ColumnName,
+                                                  txtEvidenceCode.Text);
+                if (!Success)
+                {
+                    MessageBox.Show("ชื่อพยาน/หลักฐานไม่สามารถซ้ำได้ กรุณาป้อนข้อมูลใหม่", "คำเตือน", MessageBoxButtons.OK);
+                    Success = false;
+                    return;
+                }
+            }
+
+            if (MessageBox.Show("คุณต้องการบันทึกข้อมูล ใช่หรือไม่ ?", dbConString.xMessage, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.No)
+            {
+                return;
+            }
+
+            #region
+            if (string.IsNullOrEmpty(txtPath.Text))
+            {
+                if (File.Exists(txtPath.Text))
+                {
+                    string sourcePath = Path.GetDirectoryName(txtPath.Text);
+                    string destinationPath = dbConString.SettingPath + @"\" + txtEvidenceCode.Text + @"\";
+                    string sourceFileName = Path.GetFileName(txtPath.Text);
+                    //string destinationFileName = DateTime.Now.ToString("yyyyMMddhhmmss") + ".xml"; // Don't mind this. I did this because I needed to name the copied files with respect to time.
+                    string sourceFile = System.IO.Path.Combine(sourcePath, sourceFileName);
+                    string destinationFile = System.IO.Path.Combine(destinationPath, sourceFileName);
+                    txtPath.Text = dbConString.SettingPath + @"\" + txtEvidenceCode.Text + @"\" + sourceFileName;
+
+                    if (!Directory.Exists(destinationPath))
+                    {
+                        Directory.CreateDirectory(destinationPath);
+                    }
+                    File.Copy(sourceFile, destinationFile, true);
+                }
+            }
+            #endregion
+
+            switch (FormState.ToLower())
+            {
+                case "new":
+                    #region Save
+                    if (Success)
+                    {
+                        try
+                        {
+                            #region tbUser
+                            EvidenceID = Guid.NewGuid().ToString();
+                            dbConString.Transaction = dbConString.mySQLConn.BeginTransaction();
+                            StringBuilder StringBd = new StringBuilder();
+                            StringBd.Clear();
+                            sqlTmp = string.Empty;
+                            StringBd.Append("INSERT INTO tbEvidence VALUES (@EvidenceID,@EvidenceCode,@EvidenceName,@EvidenceType,@Detail,@Path,@CreatedBy,GETDATE())");
+                            sqlTmp = "";
+                            sqlTmp = StringBd.ToString();
+
+                            dbConString.Com = new SqlCommand();
+                            dbConString.Com.CommandText = sqlTmp;
+                            dbConString.Com.CommandType = CommandType.Text;
+                            dbConString.Com.Connection = dbConString.mySQLConn;
+                            dbConString.Com.Transaction = dbConString.Transaction;
+                            dbConString.Com.Parameters.Clear();
+                            dbConString.Com.Parameters.Add("@EvidenceID", SqlDbType.VarChar).Value = EvidenceID;
+                            dbConString.Com.Parameters.Add("@EvidenceCode", SqlDbType.VarChar).Value = txtEvidenceCode.Text;
+                            dbConString.Com.Parameters.Add("@EvidenceName", SqlDbType.VarChar).Value = txtEvidenceName.Text;
+                            dbConString.Com.Parameters.Add("@EvidenceType", SqlDbType.VarChar).Value = string.Empty;
+                            dbConString.Com.Parameters.Add("@Detail", SqlDbType.VarChar).Value = txtDetail.Text;
+                            dbConString.Com.Parameters.Add("@Path", SqlDbType.VarChar).Value = txtPath.Text;
+                            dbConString.Com.Parameters.Add("@CreatedBy", SqlDbType.VarChar).Value = string.Empty;
+                            dbConString.Com.ExecuteNonQuery();
+                            dbConString.Transaction.Commit();
+                            #endregion
+                            MessageBox.Show("บันทึกค่าเรียบร้อย", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Close();
+                        }
+                        catch
+                        {
+                            dbConString.Transaction.Rollback();
+                        }
+                    }
+                    else
+                    {
+                        return;
+                    }
+                    #endregion
+                    break;
+                case "edit":
+                    #region Edit
+                    if (Success)
+                    {
+                        try
+                        {
+                            #region tbUser
+                            dbConString.Transaction = dbConString.mySQLConn.BeginTransaction();
+                            StringBuilder StringBd = new StringBuilder();
+                            StringBd.Clear();
+                            sqlTmp = string.Empty;
+                            StringBd.Append("UPDATE tbEvidence SET EvidenceCode = @EvidenceCode, EvidenceName = @EvidenceName, EvidenceType = @EvidenceType,");
+                            StringBd.Append("Detail = @Detail, Path = @Path WHERE EvidenceID = @EvidenceID");
+                            sqlTmp = "";
+                            sqlTmp = StringBd.ToString();
+
+                            dbConString.Com = new SqlCommand();
+                            dbConString.Com.CommandText = sqlTmp;
+                            dbConString.Com.CommandType = CommandType.Text;
+                            dbConString.Com.Connection = dbConString.mySQLConn;
+                            dbConString.Com.Transaction = dbConString.Transaction;
+                            dbConString.Com.Parameters.Clear();
+                            dbConString.Com.Parameters.Add("@EvidenceID", SqlDbType.VarChar).Value = EvidenceID;
+                            dbConString.Com.Parameters.Add("@EvidenceCode", SqlDbType.VarChar).Value = txtEvidenceCode.Text;
+                            dbConString.Com.Parameters.Add("@EvidenceName", SqlDbType.VarChar).Value = txtEvidenceName.Text;
+                            dbConString.Com.Parameters.Add("@EvidenceType", SqlDbType.VarChar).Value = string.Empty;
+                            dbConString.Com.Parameters.Add("@Detail", SqlDbType.VarChar).Value = txtDetail.Text;
+                            dbConString.Com.Parameters.Add("@Path", SqlDbType.VarChar).Value = txtPath.Text;
+                            dbConString.Com.ExecuteNonQuery();
+                            dbConString.Transaction.Commit();
+                            #endregion
+                            MessageBox.Show("บันทึกค่าเรียบร้อย", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            dbConString.Transaction.Rollback();
+                        }
+                    }
+                    else
+                    {
+                        return;
+                    }
+                    #endregion
+                    break;
+            }
+        }
+
+        private void btnPerview_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPath.Text))
+            {
+                if (File.Exists(txtPath.Text))
+                {
+                    frmPerview frm = new frmPerview();
+                    frm.strPath = txtPath.Text;
+                    frm.ShowDialog();
+                    btnPerview.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("ไม่พบ File ", dbConString.xMessage, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "All Files (*.*)|*.*";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = false;
+
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                string sFileName = choofdlog.FileName;
+                txtPath.Text = sFileName;
+                string[] arrAllFiles = choofdlog.FileNames; //used when Multiselect = true 
+                btnPerview.Enabled = true;
+            }
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtPath.Clear();
+            btnPerview.Enabled = false;
+        }
+    }
+}
